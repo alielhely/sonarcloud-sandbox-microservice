1bcb41309a985e018eac6fbfc5b4e6112600f9df
aelhely@qualogy.com <aelhelyqualogy.com@QLT002.home>
2025-12-18 18:04:19 +0100

    Initial comit: Spring Boot microservice template

---
+++ src/main/java/com/qlab/ai/microservice_template/AiMicroserviceTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.qlab.ai.microservice_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMicroserviceTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiMicroserviceTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qlab/ai/microservice_template/AiMicroserviceTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qlab.ai.microservice_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMicroserviceTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
