753263e94ee407db651359e94dec69841e7127f3
alielhely <35149234+alielhely@users.noreply.github.com>
2026-03-23 10:04:26 +0100

    [DEMO-129] Add Java implementation (attempt 1)

--- src/main/java/com/example/demo/DEMO129Controller.java
+++ src/main/java/com/example/demo/DEMO129Controller.java
@@ -14,12 +14,11 @@ public class DemoApplication {
 
 @RestController
 class DemoController {
-
     private static final String LOGIN_PATH = "/login";
-    private static final String LOGIN_VIEW = "login";
+    private static final String LOGIN = "login";
 
     @GetMapping(LOGIN_PATH)
     public String login() {
-        return LOGIN_VIEW;
+        return LOGIN;
     }
 }
