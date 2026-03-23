42b110f37d1e19f58c1adf0d4023280e5d2af721
alielhely <35149234+alielhely@users.noreply.github.com>
2026-03-23 13:36:04 +0100

    [DEMO-129] Add Java implementation (attempt 1)

--- src/main/java/com/example/demo/DEMO129Controller.java
+++ src/main/java/com/example/demo/DEMO129Controller.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
@@ -16,10 +15,15 @@ public class DemoApplication {
 @RestController
 class DemoController {
 
-    private static final String LOGIN_ENDPOINT = "/login";
+    private static final String LOGIN_PATH = "/login";
     private static final String LOGIN_VIEW = "login";
 
-    @GetMapping(LOGIN_ENDPOINT)
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Demo Application!";
+    }
+
+    @GetMapping(LOGIN_PATH)
     public String login() {
         return LOGIN_VIEW;
     }
